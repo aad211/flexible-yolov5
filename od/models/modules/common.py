3d8ac6a07df50959e081ebf73bb03efbdfa23a77
Muhammad As'ad Muyassir <aadaad299@gmail.com>
2022-06-06 09:35:57 +0700

    remove BatchNorm2d from Conv layer

--- od/models/modules/common.py
+++ od/models/modules/common.py
@@ -18,10 +18,11 @@ class Conv(nn.Module):
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
         super().__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
         self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
 
     def forward(self, x):
-        return self.act(self.conv(x))
+        return self.act(self.bn(self.conv(x)))
 
     def forward_fuse(self, x):
         return self.act(self.conv(x))
