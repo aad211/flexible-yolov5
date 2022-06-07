793997c32e8822170e46170be8739922cb17a06b
Muhammad As'ad Muyassir <aadaad299@gmail.com>
2022-06-07 17:58:59 +0700

    force resize on eval

--- scripts/eval.py
+++ scripts/eval.py
@@ -9,6 +9,7 @@ import torch
 import yaml
 from tqdm import tqdm
 import sys
+import torch.nn.functional as F
 sys.path.append('.')
 from torch.cuda import amp
 from od.models.modules.experimental import attempt_load
@@ -98,6 +99,7 @@ def test(data,
     loss = torch.zeros(3, device=device)
     jdict, stats, ap, ap_class, wandb_images = [], [], [], [], []
     for batch_i, (img, targets, paths, shapes) in enumerate(tqdm(dataloader, desc=s)):
+        img = F.interpolate(img, size=imgsz)
         img = img.to(device, non_blocking=True)
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
