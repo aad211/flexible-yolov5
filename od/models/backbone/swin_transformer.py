036d1bedb0b18551f3d6e0ba9f1e39adfb1bfacc
Muhammad As'ad Muyassir <aadaad299@gmail.com>
2022-06-07 07:10:32 +0700

    fix swin_transformer model

--- od/models/backbone/swin_transformer.py
+++ od/models/backbone/swin_transformer.py
@@ -622,17 +622,29 @@ class SwinTransformer(nn.Module):
     def forward(self, x):
         """Forward function."""
         x = self.patch_embed(x)
+
+        Wh, Ww = x.img_size
         if self.ape:
-            x = x + self.absolute_pos_embed
+            # interpolate the position embedding to the corresponding size
+            absolute_pos_embed = F.interpolate(self.absolute_pos_embed, size=(Wh, Ww), mode='bicubic')
+            x = (x + absolute_pos_embed).flatten(2).transpose(1, 2)  # B Wh*Ww C
+        else:
+            x = x.flatten(2).transpose(1, 2)
         x = self.pos_drop(x)
 
-        for layer in self.layers:
-            x = layer(x)
+        outs = []
+        for i in range(self.num_layers):
+            layer = self.layers[i]
+            x_out, H, W, x, Wh, Ww = layer(x, Wh, Ww)
 
-        x = self.norm(x)  # B L C
-        x = self.avgpool(x.transpose(1, 2))  # B C 1
-        x = torch.flatten(x, 1)
-        return x
+            if i in self.out_indices:
+                norm_layer = getattr(self, f'norm{i}')
+                x_out = norm_layer(x_out)
+
+                out = x_out.view(-1, H, W, self.num_features[i]).permute(0, 3, 1, 2).contiguous()
+                outs.append(out)
+
+        return outs[1:]
 
     def train(self, mode=True):
         """Convert the model into training mode while keep layers freezed."""
